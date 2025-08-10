9bfc149803864d046a4de79e0fdaec7714aae49d
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2025-08-10 23:27:59 +0300

    added overlay for cart and filter lists

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -37,18 +37,25 @@ export default function NavBar({AllProductsInfo, filters, setFilters , query, se
                         {isOpenFilter ? <span>Close&nbsp;</span> : "Filters"}
                     </button>
                     {isOpenFilter && (
-                        <FiltersPage
-                            AllProductsInfo={AllProductsInfo}
-                            filters={filters}
-                            setFilters={setFilters}
-                        />
+                        <>
+                            <div
+                                className="fixed inset-0 bg-black bg-opacity-50 z-40"
+                                onClick={() => setIsOpenFilter(false)}
+                                >
+                            </div>
+                            <FiltersPage className="z-50"
+                                AllProductsInfo={AllProductsInfo}
+                                filters={filters}
+                                setFilters={setFilters}
+                            />
+                        </>
                     )}
             </div>
         </div>
 
             <div className="relative">
                 <button
-                    className={`px-4 py-2 rounded-lg transition font-medium ${
+                    className={`-mt-0.5 px-4 py-2 rounded-lg transition font-medium ${
                         isOpenCart
                             ? "bg-red-500 text-white hover:bg-red-600"
                             : "bg-gray-100 text-gray-800 hover:bg-gray-200"
@@ -69,10 +76,17 @@ export default function NavBar({AllProductsInfo, filters, setFilters , query, se
                 </button>
 
                 {isOpenCart && (
-                    <CartList
-                        products={products}
-                        setProducts={setProducts}
-                    />
+                    <>
+                        <div
+                            className="fixed inset-0 bg-black bg-opacity-50 z-40"
+                            onClick={() => setIsOpenCart(false)}
+                            >
+                        </div>
+                        <CartList className="z-50"
+                            products={products}
+                            setProducts={setProducts}
+                        />
+                    </>
                 )}
             </div>
     </div>
